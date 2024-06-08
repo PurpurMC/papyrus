cfb677fe79a8b2e265032fbe1d65873bde46e864
Ben Kerllenevich <ben@omega24.dev>
2024-06-08 16:52:20 -0400

    better sql queries

--- src/main/java/org/purpurmc/papyrus/db/repository/BuildRepository.java
+++ src/main/java/org/purpurmc/papyrus/db/repository/BuildRepository.java
@@ -16,17 +16,17 @@ public interface BuildRepository extends JpaRepository<Build, UUID> {
     boolean existsByVersionAndName(Version version, String name);
 
     @Query("SELECT b FROM Build b WHERE b.version = :version AND b.name = :name AND b.ready = 1")
-    Optional<Build> findByVersionAndNameAndReady(Version version, String name);
+    Optional<Build> findByVersionAndNameAndReady(@Param("version") Version version, String name);
 
     @Query("SELECT b FROM Build b WHERE b.version = :version AND b.ready = 1 ORDER BY timestamp ASC")
-    List<Build> findAllByVersionAndReadyOrderByTimestampAsc(Version version);
+    List<Build> findAllByVersionAndReadyOrderByTimestampAsc(@Param("version") Version version);
 
     @Query("SELECT b FROM Build b WHERE b.version = :version AND b.name = :name AND b.ready = 1 AND b.result = 'SUCCESS'")
-    Optional<Build> findByVersionAndNameAndReadyAndResultIsSuccess(Version version, String name);
+    Optional<Build> findByVersionAndNameAndReadyAndResultIsSuccess(@Param("version") Version version, String name);
 
-    @Query("SELECT b FROM Build b WHERE b.version = :version AND b.name = :name AND b.file IS NOT null AND b.result = 'SUCCESS'")
-    Optional<Build> findByVersionAndNameAndFileNotNullAndResultIsSuccess(Version version, String name);
+    @Query("SELECT b FROM Build b WHERE b.version = :version AND b.name = :name  AND b.file IS NOT null AND b.ready = 1 AND b.result = 'SUCCESS'")
+    Optional<Build> findByVersionAndNameAndFileNotNullAndResultIsSuccess(@Param("version") Version version, String name);
 
-    @Query("SELECT b FROM Build b WHERE b.version = :version AND b.file IS NOT null AND b.result = 'SUCCESS' ORDER BY timestamp DESC LIMIT 1")
-    Optional<Build> findLatestByVersionAndFileNotNull(Version version);
+    @Query("SELECT b FROM Build b WHERE b.version = :version AND b.file IS NOT null AND b.ready = 1 AND b.result = 'SUCCESS' ORDER BY timestamp DESC LIMIT 1")
+    Optional<Build> findLatestByVersionAndFileNotNull(@Param("version") Version version);
 }
